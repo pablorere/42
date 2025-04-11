2094a3e5b56aa1dae052528061c2371b66f8d655
Pablo Renato Paula Souza <ppaula-s@c3r2s1.42urduliz.com>
2025-04-11 23:53:16 +0200

    viernesito

---
+++ mile0/libft/done/libft.h
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ppaula-s <ppaula-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/07 15:22:44 by ppaula-s          #+#    #+#             */
+/*   Updated: 2025/04/11 23:50:22 by ppaula-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+int		ft_isalpha(int c );
+
+int		ft_isascii(int c);
+
+int		ft_isalnum(int c);
+
+int		ft_isdigit(int c);
+
+int		ft_isprint(int c);
+
+int		ft_atoi(const char *nptr);
+
+void	*ft_memset(void *s, int c, size_t n);
+
+void	ft_bzero(void *s, size_t n);
+
+void	*ft_calloc(size_t nmemb, size_t size);
+
+int		ft_toupper(int c);
+
+int		ft_tolower(int c);
+
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+
+size_t	ft_strlen(const char *s);
+
+void	ft_putchar_fd(char c, int fd);
+
+void	ft_putnbr_fd(int n, int fd);
+
+void	ft_putstr_fd(char *s, int fd);
+
+void	ft_putendl_fd(char *s, int fd);
+
+#endif
