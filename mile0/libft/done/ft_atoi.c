8a59bc4caa43d61833175fa89839eea4df0e44aa
pablorere <65947059+pablorere@users.noreply.github.com>
2025-04-14 10:13:35 +0200

    Update ft_atoi.c

--- mile0/libft/done/ft_atoi.c
+++ mile0/libft/done/ft_atoi.c
@@ -10,7 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h> 
+
+int ft_isdigit(int num)
+{
+	return (num >= '0' && num <= '9');
+}
 
 int	ft_atoi(const char *nptr)
 {
@@ -19,26 +26,22 @@ int	ft_atoi(const char *nptr)
 	unsigned int	n;
 
 	i = 0;
-	sign = 1;
 	n = 0;
 	while (nptr[i] == 32 || (nptr[i] >= 9 && nptr[i] <= 13))
 		i++;
-	if (nptr[i] == '+' || nptr[i] == '-')
-	{
-		if (nptr[i] == '-')
-			sign = -1;
-		i++;
-	}
+	sign = 1 - 2 * (nptr[i] == '-');
+	i += nptr[i] == '+' || nptr[i] == '-';
 	while (ft_isdigit(nptr[i]))
 	{
 		if (n > INT_MAX / 10
 			|| (n == INT_MAX / 10 && (nptr[i] - '0') > INT_MAX % 10))
-		{
-			if (sign == 1)
-				return (-1);
-			return (0);
-		}
+			return (-(sign == 1));
 		n = n * 10 + (nptr[i++] - '0');
 	}
 	return ((int)(n * sign));
+}
+int	main(int ac, char **av)
+{
+	printf("la originalllll: %d\n", atoi(av[1]));
+	printf("la mia: %d\n", ft_atoi(av[1]));
 }
