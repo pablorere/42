89dab4dd427239dad8f3f7443d7090497722aeca
Pablo Renato Paula Souza <ppaula-s@c3r1s1.42urduliz.com>
2025-04-14 16:14:10 +0200

    finalmente lo hjice

--- mile0/libft/done/libft.h
+++ mile0/libft/done/libft.h
@@ -6,7 +6,7 @@
 /*   By: ppaula-s <ppaula-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 15:22:44 by ppaula-s          #+#    #+#             */
-/*   Updated: 2025/04/14 15:57:21 by ppaula-s         ###   ########.fr       */
+/*   Updated: 2025/04/14 16:07:27 by ppaula-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,8 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 
 void	*ft_memmove(void *dest, const void *src, size_t n);
+
+void	*ft_memchr(const void *s, int c, size_t n);
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
