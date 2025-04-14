9719589d5660a5f63a68f48ba60fe62814c7ef1a
Pablo Renato Paula Souza <ppaula-s@c3r1s1.42urduliz.com>
2025-04-14 16:03:03 +0200

    memmove, memcpy

--- mile0/libft/done/libft.h
+++ mile0/libft/done/libft.h
@@ -6,7 +6,7 @@
 /*   By: ppaula-s <ppaula-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 15:22:44 by ppaula-s          #+#    #+#             */
-/*   Updated: 2025/04/12 00:26:45 by ppaula-s         ###   ########.fr       */
+/*   Updated: 2025/04/14 15:57:21 by ppaula-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
+# include <stddef.h>
 
 int		ft_isalpha(int c );
 
@@ -40,6 +41,10 @@ int		ft_toupper(int c);
 int		ft_tolower(int c);
 
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+
+void	*ft_memmove(void *dest, const void *src, size_t n);
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
