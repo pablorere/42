65fd1ff26e60a8038ff147392db5ceddfc3cce7c
pablorere <pablorere201787@gmail.com>
2025-08-09 18:00:34 +0200

    fixing

--- mile3/philosophers/src/init.c
+++ mile3/philosophers/src/init.c
@@ -7,15 +7,3 @@ void init_sim(t_table *table)
 }
 /*safe mutexes functions*/
 
-void safe_handle_mutex(t_mtx *mutex, t_opcode opcode)
-{
-	if (opcode == LOCK)
-		pthread_mutex_lock(mutex);
-	else if (opcode == UNLOCK)
-		pthread_mutex_unlock(mutex);
-	else if (opcode == INIT)
-		pthread_mutex_init(mutex, NULL);
-	else if (opcode == DESTROY)
-		pthread_mutex_destroy(mutex);
-	
-}
